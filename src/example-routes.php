1c3952cefcbc3ce020666eb416f9ec566d8886f8
Aaron Adrian <aaroncadrian@gmail.com>
2018-02-23 16:51:58 -0600

    Example routes for testing purposes

---
+++ src/example-routes.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::get('/example-show', function() {
+    return;
+})->name('example.show');
+
+Route::post('/example-store/{form}', function() {
+    return;
+})->name('example.store');
