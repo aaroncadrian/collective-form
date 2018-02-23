89366697cddbf2e46860d5e8841cf94127a86131
Aaron Adrian <aaroncadrian@gmail.com>
2018-02-23 16:51:26 -0600

    Updated default functionality

--- src/Contracts/Form.php
+++ src/Contracts/Form.php
@@ -6,15 +6,17 @@ use Illuminate\Contracts\Support\Arrayable;
 
 interface Form extends Arrayable
 {
-    public function post($uri, array $options = []);
+    public function post($uri = '', array $options = []);
 
-    public function get($uri, array $options = []);
+    public function get($uri = '', array $options = []);
 
-    public function patch($uri, array $options = []);
+    public function patch($uri = '', array $options = []);
 
-    public function put($uri, array $options = []);
+    public function put($uri = '', array $options = []);
 
-    public function delete($uri, array $options = []);
+    public function delete($uri = '', array $options = []);
+
+    public function route($uri, array $options = []);
 
     public function files($files = true);
 
