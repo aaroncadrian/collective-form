6138c8e4aa5d1d3d92807bbecbfe7a89543861eb
Aaron Adrian <aaroncadrian@gmail.com>
2018-02-23 18:27:06 -0600

    Added opener macro

--- src/CollectiveFormServiceProvider.php
+++ src/CollectiveFormServiceProvider.php
@@ -3,6 +3,8 @@
 namespace AaronAdrian\CollectiveForm;
 
 use AaronAdrian\CollectiveForm\Contracts\FormContract;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Support\ServiceProvider;
 
 class CollectiveFormServiceProvider extends ServiceProvider
@@ -13,7 +15,38 @@ class CollectiveFormServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(FormContract::class, FormHelper::class);
+        $this->registerRoutes();
+        $this->registerOpenerMacro();
+    }
 
+    protected function registerOpenerMacro()
+    {
+        $builder = app('form');
+
+        $builder->macro('opener', function($opener) use ($builder) {
+            return $builder->open(value(function() use($opener) {
+
+                if($opener instanceof Arrayable) {
+                    return $opener->toArray();
+                }
+
+                if(is_array($opener))
+                {
+                    return $opener;
+                }
+
+                if($opener instanceof Renderable) {
+                    return $opener->render();
+                }
+
+                throw new \Exception('Object for Form::opener() is not an array, Arrayable, or Renderable.');
+
+            }));
+        });
+    }
+
+    protected function registerRoutes()
+    {
         if($this->app->environment('testing'))
         {
             $this->loadRoutesFrom(__DIR__.'/example-routes.php');
