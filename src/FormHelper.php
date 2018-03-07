0f8544dd1e03af67439901d21b156b856107f3ac
Aaron Adrian <aaroncadrian@gmail.com>
2018-03-07 13:20:55 -0600

    Move when logic out of FormHelper class and into trait

--- src/FormHelper.php
+++ src/FormHelper.php
@@ -150,30 +150,6 @@ class FormHelper implements FormContract
         return $this;
     }
 
-    /**
-     * Register a WhenCase.
-     *
-     * @param bool|callable $condition
-     * @param callable $ifTrue
-     * @param callable|null $ifFalse
-     * @return $this
-     */
-    public function when($condition, callable $ifTrue, callable $ifFalse = null)
-    {
-        $this->whenCases->push(new WhenCase($this, $condition, $ifTrue, $ifFalse));
-        return $this;
-    }
 
-    /**
-     * Handle each WhenCase.
-     *
-     * @return void
-     */
-    protected function renderWhenCases()
-    {
-        $this->whenCases->each(function(WhenCase $case) {
-            $case->handle();
-        });
-    }
 
 }
