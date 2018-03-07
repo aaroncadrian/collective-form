a18161b1ca2d1591462459cfe62b024a90be7fff
Aaron Adrian <aaroncadrian@gmail.com>
2018-03-07 16:47:34 -0600

    Add test to see if form opener works

--- tests/Feature/FormOpenerTest.php
+++ tests/Feature/FormOpenerTest.php
@@ -2,7 +2,20 @@
 
 namespace AaronAdrian\CollectiveForm\Tests\Feature;
 
-class FormOpenerTest
-{
+use AaronAdrian\CollectiveForm\Facades\Form;
+use AaronAdrian\CollectiveForm\Tests\TestCase;
+use Collective\Html\FormFacade;
 
+class FormOpenerTest extends TestCase
+{
+    /**
+     * @test
+     * @group Opener
+     */
+    public function post_with_opener()
+    {
+        $form = Form::post()->url('https://www.testing.com');
+        $opener = (string) FormFacade::opener($form);
+        $this->assertEquals('<form method="POST" action="https://www.testing.com" accept-charset="UTF-8"><input name="_token" type="hidden">', $opener);
+    }
 }
