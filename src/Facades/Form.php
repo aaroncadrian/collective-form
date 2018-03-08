e680695ce7f698db25aed6cefe65af8e6f5813ab
Aaron Adrian <aaroncadrian@gmail.com>
2018-03-08 00:00:15 -0600

    Add doc blocks for Form facade

--- src/Facades/Form.php
+++ src/Facades/Form.php
@@ -6,6 +6,14 @@ use Illuminate\Support\Facades\Facade;
 
 class Form extends Facade
 {
+    /**
+     * Get facade accessor for Form.
+     *
+     * @see \AaronAdrian\CollectiveForm\CollectiveFormServiceProvider::registerFormHelper()
+     * @see \AaronAdrian\CollectiveForm\Services\FormHelper
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'form-helper';
